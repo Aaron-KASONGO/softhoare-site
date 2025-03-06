1916119e7122adc82d02dc5fa47012b69c5b6b34
Aaron KASONGO <19kk181@esisalama.org>
2025-03-06 06:53:50 +0100

    adding something

---
+++ app/_components/features-section.tsx
@@ -0,0 +1,97 @@
+import React from 'react'
+
+export const FeaturesSection = () => {
+    
+  return (
+    <>
+        <div className='bg-slate-100'>
+
+            <div className="container space-y-8 px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+                <div className="max-w-3xl">
+                    <h3 className="text-green-600 font-semibold">
+                        Features
+                    </h3>
+                    <h2 className="text-slate-800 font-semibold text-2xl md:text-4xl md:leading-tight">Our approach</h2>
+                    <p className="mt-1 text-neutral-500">This profound insight guides our comprehensive strategy — from meticulous research and strategic planning to the seamless execution of brand development and website or product deployment.</p>
+                </div>
+
+                <div className="grid sm:grid-cols-2 lg:grid-cols-3 items-center gap-6 md:gap-10">
+
+                    <div className="size-full hover:bg-slate-50 hover:shadow-lg rounded-lg transition-shadow duration-300 group p-5">
+                        <div className="flex items-center gap-x-4 mb-3">
+                            <div className="inline-flex justify-center items-center size-15.5 rounded-full border-4 border-blue-50 bg-blue-100">
+                            <svg className="shrink-0 size-6 text-slate-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="13.5" cy="6.5" r=".5"/><circle cx="17.5" cy="10.5" r=".5"/><circle cx="8.5" cy="7.5" r=".5"/><circle cx="6.5" cy="12.5" r=".5"/><path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10c.926 0 1.648-.746 1.648-1.688 0-.437-.18-.835-.437-1.125-.29-.289-.438-.652-.438-1.125a1.64 1.64 0 0 1 1.668-1.668h1.996c3.051 0 5.555-2.503 5.555-5.554C21.965 6.012 17.461 2 12 2z"/></svg>
+                            </div>
+                            <div className="shrink-0">
+                            <h3 className="block text-lg font-semibold text-slate-800">Build your portfolio</h3>
+                            </div>
+                        </div>
+                        <p className="text-slate-600">The simplest way to keep your portfolio always up-to-date.</p>
+                    </div>
+
+                    <div className="size-full hover:bg-slate-50 hover:shadow-lg rounded-lg transition-shadow duration-300 group p-5">
+                        <div className="flex items-center gap-x-4 mb-3">
+                            <div className="inline-flex justify-center items-center size-15.5 rounded-full border-4 border-blue-50 bg-blue-100">
+                            <svg className="shrink-0 size-6 text-slate-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="13.5" cy="6.5" r=".5"/><circle cx="17.5" cy="10.5" r=".5"/><circle cx="8.5" cy="7.5" r=".5"/><circle cx="6.5" cy="12.5" r=".5"/><path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10c.926 0 1.648-.746 1.648-1.688 0-.437-.18-.835-.437-1.125-.29-.289-.438-.652-.438-1.125a1.64 1.64 0 0 1 1.668-1.668h1.996c3.051 0 5.555-2.503 5.555-5.554C21.965 6.012 17.461 2 12 2z"/></svg>
+                            </div>
+                            <div className="shrink-0">
+                            <h3 className="block text-lg font-semibold text-slate-800">Build your portfolio</h3>
+                            </div>
+                        </div>
+                        <p className="text-slate-600">The simplest way to keep your portfolio always up-to-date.</p>
+                    </div>
+
+                    <div className="size-full hover:bg-slate-50 hover:shadow-lg rounded-lg transition-shadow duration-300 group p-5">
+                        <div className="flex items-center gap-x-4 mb-3">
+                            <div className="inline-flex justify-center items-center size-15.5 rounded-full border-4 border-blue-50 bg-blue-100">
+                            <svg className="shrink-0 size-6 text-slate-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="13.5" cy="6.5" r=".5"/><circle cx="17.5" cy="10.5" r=".5"/><circle cx="8.5" cy="7.5" r=".5"/><circle cx="6.5" cy="12.5" r=".5"/><path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10c.926 0 1.648-.746 1.648-1.688 0-.437-.18-.835-.437-1.125-.29-.289-.438-.652-.438-1.125a1.64 1.64 0 0 1 1.668-1.668h1.996c3.051 0 5.555-2.503 5.555-5.554C21.965 6.012 17.461 2 12 2z"/></svg>
+                            </div>
+                            <div className="shrink-0">
+                            <h3 className="block text-lg font-semibold text-slate-800">Build your portfolio</h3>
+                            </div>
+                        </div>
+                        <p className="text-slate-600">The simplest way to keep your portfolio always up-to-date.</p>
+                    </div>
+
+                    <div className="size-full hover:bg-slate-50 hover:shadow-lg rounded-lg transition-shadow duration-300 group p-5">
+                        <div className="flex items-center gap-x-4 mb-3">
+                            <div className="inline-flex justify-center items-center size-15.5 rounded-full border-4 border-blue-50 bg-blue-100">
+                            <svg className="shrink-0 size-6 text-slate-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="13.5" cy="6.5" r=".5"/><circle cx="17.5" cy="10.5" r=".5"/><circle cx="8.5" cy="7.5" r=".5"/><circle cx="6.5" cy="12.5" r=".5"/><path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10c.926 0 1.648-.746 1.648-1.688 0-.437-.18-.835-.437-1.125-.29-.289-.438-.652-.438-1.125a1.64 1.64 0 0 1 1.668-1.668h1.996c3.051 0 5.555-2.503 5.555-5.554C21.965 6.012 17.461 2 12 2z"/></svg>
+                            </div>
+                            <div className="shrink-0">
+                            <h3 className="block text-lg font-semibold text-slate-800">Build your portfolio</h3>
+                            </div>
+                        </div>
+                        <p className="text-slate-600">The simplest way to keep your portfolio always up-to-date.</p>
+                    </div>
+
+                    <div className="size-full hover:bg-slate-50 hover:shadow-lg rounded-lg transition-shadow duration-300 group p-5">
+                        <div className="flex items-center gap-x-4 mb-3">
+                            <div className="inline-flex justify-center items-center size-15.5 rounded-full border-4 border-blue-50 bg-blue-100">
+                            <svg className="shrink-0 size-6 text-slate-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="13.5" cy="6.5" r=".5"/><circle cx="17.5" cy="10.5" r=".5"/><circle cx="8.5" cy="7.5" r=".5"/><circle cx="6.5" cy="12.5" r=".5"/><path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10c.926 0 1.648-.746 1.648-1.688 0-.437-.18-.835-.437-1.125-.29-.289-.438-.652-.438-1.125a1.64 1.64 0 0 1 1.668-1.668h1.996c3.051 0 5.555-2.503 5.555-5.554C21.965 6.012 17.461 2 12 2z"/></svg>
+                            </div>
+                            <div className="shrink-0">
+                            <h3 className="block text-lg font-semibold text-slate-800">Build your portfolio</h3>
+                            </div>
+                        </div>
+                        <p className="text-slate-600">The simplest way to keep your portfolio always up-to-date.</p>
+                    </div>
+
+                    <div className="size-full hover:bg-slate-50 hover:shadow-lg rounded-lg transition-shadow duration-300 group p-5">
+                        <div className="flex items-center gap-x-4 mb-3">
+                            <div className="inline-flex justify-center items-center size-15.5 rounded-full border-4 border-blue-50 bg-blue-100">
+                            <svg className="shrink-0 size-6 text-slate-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="13.5" cy="6.5" r=".5"/><circle cx="17.5" cy="10.5" r=".5"/><circle cx="8.5" cy="7.5" r=".5"/><circle cx="6.5" cy="12.5" r=".5"/><path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10c.926 0 1.648-.746 1.648-1.688 0-.437-.18-.835-.437-1.125-.29-.289-.438-.652-.438-1.125a1.64 1.64 0 0 1 1.668-1.668h1.996c3.051 0 5.555-2.503 5.555-5.554C21.965 6.012 17.461 2 12 2z"/></svg>
+                            </div>
+                            <div className="shrink-0">
+                            <h3 className="block text-lg font-semibold text-slate-800">Build your portfolio</h3>
+                            </div>
+                        </div>
+                        <p className="text-slate-600">The simplest way to keep your portfolio always up-to-date.</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
---
+++ app/_components/testimonial.tsx
@@ -0,0 +1,113 @@
+import React from 'react'
+
+export const Testimonial = () => {
+  return (
+    <>
+        <section className="py-10 lg:py-20 px-4 sm:px-6 lg:px-8 bg-gray-50">
+            <div className="container mx-auto">
+
+                <div className="max-w-3xl mb-10 lg:mb-14">
+                    <h3 className="text-green-600 font-semibold">
+                        Features
+                    </h3>
+                    <h2 className="text-slate-800 font-semibold text-2xl md:text-4xl md:leading-tight">Our approach</h2>
+                    <p className="mt-1 text-neutral-500">This profound insight guides our comprehensive strategy — from meticulous research and strategic planning to the seamless execution of brand development and website or product deployment.</p>
+                </div>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+
+                <div className="bg-white rounded-2xl shadow-lg p-8 hover:shadow-xl transition-all duration-300 group relative">
+                    <div className="absolute top-0 right-0 w-16 h-16 z-50 to-transparent rounded-tr-2xl"></div>
+                        <div className="flex items-start gap-4 mb-6">
+                            <img 
+                                src="https://i.pinimg.com/736x/ad/92/97/ad92977e0f56d1fea0dde144598640e0.jpg" 
+                                alt="Marie Dupont" 
+                                className="w-14 h-14 object-cover rounded-full border-2 border-white shadow-md group-hover:scale-105 transition-transform"
+                            />
+                            <div>
+                                <h4 className="font-bold text-gray-900">Marie Dupont</h4>
+                                <p className="text-sm text-gray-600">CEO @TechStart</p>
+                            </div>
+                            <img 
+                                src="https://i.pinimg.com/736x/0b/81/b1/0b81b10dd625feda99500d7f244f7e84.jpg" 
+                                alt="Logo TechStart" 
+                                className="absolute object-cover top-0 right-0 w-16 h-16 rounded-tr-2xl rounded-bl-2xl ml-auto opacity-100"
+                            />
+                        </div>
+                        {/* <div className="mb-4 flex text-amber-400">
+                        ★★★★★
+                        </div> */}
+                        <p className="text-gray-700 relative z-10">
+                        "Une plateforme révolutionnaire qui a transformé notre façon de collaborer. L'interface intuitive et le support réactif font toute la différence !"
+                        </p>
+                    {/* <div className="flex items-center justify-between text-sm">
+                    <span className="text-gray-500">12 projets réalisés</span>
+                    <span className="text-blue-600 font-medium">Lire plus →</span>
+                    </div> */}
+                </div>
+
+                <div className="bg-white rounded-2xl shadow-lg p-8 hover:shadow-xl transition-all duration-300 group relative">
+                    <div className="absolute top-0 right-0 w-16 h-16 z-50 to-transparent rounded-tr-2xl"></div>
+                    <div className="flex items-start gap-4 mb-6">
+                    <img 
+                        src="https://i.pinimg.com/736x/81/ac/9e/81ac9ef6456e8eccbf651fc368e83e4a.jpg" 
+                        alt="Pierre Martin" 
+                        className="w-14 h-14 object-cover rounded-full border-2 border-white shadow-md group-hover:scale-105 transition-transform"
+                    />
+                    <div>
+                        <h4 className="font-bold text-gray-900">Pierre Martin</h4>
+                        <p className="text-sm text-gray-600">Directeur Marketing</p>
+                    </div>
+                    <img 
+                        src="https://i.pinimg.com/736x/26/e9/08/26e908aebdcd86de9cbcdc2571e43847.jpg" 
+                        alt="Logo entreprise" 
+                        className="absolute object-cover top-0 right-0 w-16 h-16 rounded-tr-2xl rounded-bl-2xl ml-auto opacity-100"
+                    />
+                    </div>
+                    {/* <div className="mb-4 flex text-amber-400">
+                    ★★★★☆
+                    </div> */}
+                    <p className="text-gray-700 relative z-10">
+                    "Des résultats mesurables dès le premier mois d'utilisation. La personnalisation des outils a dépassé nos attentes."
+                    </p>
+                    {/* <div className="flex items-center justify-between text-sm">
+                    <span className="text-gray-500">8 mois d'utilisation</span>
+                    <span className="text-blue-600 font-medium">Lire plus →</span>
+                    </div> */}
+                </div>
+
+                <div className="bg-white rounded-2xl shadow-lg p-8 hover:shadow-xl transition-all duration-300 group relative">
+                    <div className="absolute top-0 right-0 w-16 h-16 z-50 to-transparent rounded-tr-2xl"></div>
+                    <div className="flex items-start gap-4 mb-6">
+                    <img 
+                        src="https://i.pinimg.com/736x/e6/77/d5/e677d532074ad286dc60b9a915e4adee.jpg" 
+                        alt="Sophie Leroy" 
+                        className="w-14 h-14 object-cover rounded-full border-2 border-white shadow-md group-hover:scale-105 transition-transform"
+                    />
+                    <div>
+                        <h4 className="font-bold text-gray-900">Sophie Leroy</h4>
+                        <p className="text-sm text-gray-600">CTO @StartupX</p>
+                    </div>
+                    <img 
+                        src="https://i.pinimg.com/736x/ef/23/f7/ef23f7ac15dd7e2f8c545649503f1c71.jpg" 
+                        alt="Logo StartupX" 
+                        className="absolute object-cover top-0 right-0 w-16 h-16 rounded-tr-2xl rounded-bl-2xl ml-auto opacity-100"
+                    />
+                    </div>
+                    {/* <div className="mb-4 flex text-amber-400">
+                    ★★★★★
+                    </div> */}
+                    <p className="text-gray-700 relative z-10">
+                    "L'intégration parfaite avec nos outils existants et une équipe toujours à l'écoute. Un partenaire technologique de premier choix."
+                    </p>
+                    {/* <div className="flex items-center justify-between text-sm">
+                    <span className="text-gray-500">25 collaborateurs</span>
+                    <span className="text-blue-600 font-medium">Lire plus →</span>
+                    </div> */}
+                </div>
+                </div>
+            </div>
+        </section>
+    </>
+  )
+}
