372ee6795f40e111e15e016a642416c502acc4b9
Aaron KASONGO <19kk181@esisalama.org>
2025-03-14 00:31:28 +0100

    adding something

---
+++ app/phylosophie/layout.tsx
@@ -0,0 +1,34 @@
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+    title: "SoftHoare | Projets",
+    description: "Découvrez nos réalisations, des projets innovants et sur mesure qui témoignent de notre expertise et de notre capacité à transformer vos idées en solutions concrètes et performantes.",
+    robots: "index, follow",
+    keywords: ["Agence", "Agence developpement", "developpement logiciel", "lubumbashi", "lubumbashi developpement", "devlopment", "web app", "mobile app", "software"],
+    openGraph: {
+      title: "SoftHoare | Accueil",
+      description: "Découvrez nos réalisations, des projets innovants et sur mesure qui témoignent de notre expertise et de notre capacité à transformer vos idées en solutions concrètes et performantes.",
+      url: "https://softhoare.com/projets",
+      images: "/logos/logo_blue.png"
+    },
+    twitter: {
+      title: "SoftHoare | Accueil",
+      description: "Découvrez nos réalisations, des projets innovants et sur mesure qui témoignent de notre expertise et de notre capacité à transformer vos idées en solutions concrètes et performantes.",
+      card: "summary",
+      images: "/logos/logo_blue.png",
+        site: "https://softhoare.com/projets"
+    }
+};
+  
+  export default function Layout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+  }
